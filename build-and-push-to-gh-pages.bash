10d2536546f2063477d7c919a8da409ffc925416
Yann Salaün <yann.salaun@ulule.com>
2018-06-08 17:35:07 +0200

    add build-and-push-to-gh-pages.bash script

---
+++ build-and-push-to-gh-pages.bash
@@ -0,0 +1,10 @@
+#!/bin/bash -eo pipefail
+
+rm -fr public
+git worktree add -B gh-pages public origin/gh-pages
+hugo
+git -C public add --all
+git config user.email "you@example.com"
+git config user.name "Your Name"
+git -C public commit -m "Publishing to gh-pages" || exit 0
+git push origin gh-pages
